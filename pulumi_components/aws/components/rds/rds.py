564aa26ab5be73df0162882eea53866d9a1de587
Mohammad Ayub <asimayub81@gmail.com>
2022-12-29 17:15:24 +0000

    added security-group for rds

--- pulumi_components/aws/components/rds/rds.py
+++ pulumi_components/aws/components/rds/rds.py
@@ -1,48 +1,80 @@
-from typing import Sequence, Optional
+from typing import Sequence, Optional, Mapping
 
 import pulumi
+import pulumi_aws as aws
 from pulumi import ComponentResource
+from _inputs import RdsSecurityGroupIngressArgs
 
-
-class RDSCommonAttrs:
-    """A class defining common rds attributes"""
+class RdsSecurityGroup(ComponentResource):
+    """A class creating an RDS security resource"""
 
     def __init__(self,
-                 family: str,
-                 engine: str,
-                 engine_version: str,
-                 master_password: str,
+                 name: str,
+                 db_engine: str,
                  vpc_id: str,
-                 subnet_ids: pulumi.Input[Sequence[str]],
-                 additional_security_group_ids: Optional[pulumi.Input[Sequence[str]]] = [],
-                 ingress_security_group_cidrs: Optional[pulumi.Input[Sequence[str]]] = None,
-                 ingress_security_group_ids: Optional[pulumi.Input[str]] = None,
-                 backup_retention_period: int = 14,
-                 maintenance_window: str = "sun:01:00-sun:03:00",
-                 backup_window: str = "03:00-05:00",
-                 db_name: str = ''
+                 ingress_security_group_cidrs: Optional[pulumi.Input[Sequence[str]]] = [],
+                 ingress_security_group_ids: Optional[pulumi.Input[Sequence[str]]] = None,
+                 opts: Optional[pulumi.ResourceOptions] = None,
                  ):
-        self.family = family
-        self.engine = engine
-        self.engine_version = engine_version
-        self.master_password = master_password
-        self.vpc_id = vpc_id
-        self.subnet_ids = subnet_ids
-        self.additional_security_group_ids = additional_security_group_ids
-        self.ingress_security_group_cidrs = ingress_security_group_cidrs
-        self.ingress_security_group_ids = ingress_security_group_ids
-        self.backup_retention_period = backup_retention_period
-        self.maintenance_window = maintenance_window
-        self.backup_window = backup_window
-        self.db_name = db_name
+        super().__init__("pulumi-components:aws:components:rds-security-group", name, {}, opts)
+        self.security_group = aws.ec2.SecurityGroup(
+            f"{name}-security-group",
+            description=f"{name} security-group",
+            vpc_id=vpc_id,
+            ingress=RdsSecurityGroupIngressArgs(
+                db_engine,
+
+            )
+        )
 
 
-class RDSInstance(ComponentResource, RDSCommonAttrs):
+class RDSInstance(ComponentResource):
     """A class defining an RDS instance custom resource"""
+
     def __init__(self,
+                 name: str,
                  allocated_storage: str,
                  instance_class: str,
+                 engine: str,
+                 engine_version: str,
+                 identifier: str,
+                 identifier_prefix: str,
+                 password: str,
                  allow_major_version_upgrade: bool = False,
                  apply_immediately: bool = False,
                  auto_minor_version_upgrade: bool = False,
+                 backup_retention_period: int = 14,
+                 backup_window: str = "03:00-05:00",
+                 blue_green_update: aws.rds.InstanceBlueGreenUpdateArgs = None,
+                 ca_cert_identifier: str = None,
+                 character_set_name: str = "utf8",
+                 copy_tags_to_snapshot: bool = True,
+                 custom_iam_instance_profile: str = None,
+                 customer_owned_ip_enabled: bool = False,
+                 db_name: str = None,
+                 delete_automated_backups: bool = True,
+                 deletion_protection: bool = True,
+                 domain: str = None,
+                 domain_iam_role_name: str = None,
+                 enabled_cloudwatch_logs_exports: Sequence[str] = [],
+                 final_snapshot_identifier: str = '',
+                 iam_database_authentication_enabled: bool = False,
+                 kms_key_id: str = None,
+                 maintenance_window: str = "sun:01:00-sun:03:00",
+                 max_allocated_storage: int = 100,
+                 monitoring_interval: int = 0,
+                 monitoring_role_arn: str = None,
+                 multi_az: bool = False,
+                 performance_insights_enabled: bool = False,
+                 performance_insights_kms_key_id: str = None,
+                 performance_insights_retention_period: int = 7,
+                 publicly_accessible: bool = False,
+                 skip_final_snapshot: bool = True,
+                 storage_encrypted: bool = False,
+                 tags: Mapping[str, str] = {},
+                 additional_vpc_security_group_ids: Optional[Sequence[str]] = [],
+                 ingress_security_group_cidrs: Optional[pulumi.Input[Sequence[str]]] = [],
+                 ingress_security_group_ids: Optional[pulumi.Input[Sequence[str]]] = None,
+                 opts: Optional[pulumi.ResourceOptions] = None,
                  ):
+        super().__init__(""pulumi-components:aws:components:rdsInstance"", name, {}, opts)
